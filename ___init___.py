80ef82fee0c2cafe03cc30377be79c1e0c4f6049
wuxingyu <2089622020@qq.com>
2025-12-02 19:58:45 +0800

    Delete ___init___.py

--- ___init___.py
+++
@@ -1,6 +0,0 @@
-from pettingllms. multi_agent_env.deep_search. agents.q_agent import QAgent
-from pettingllms.multi_agent_env.deep_search.agents. r_agent import RAgent
-from pettingllms.multi_agent_env.deep_search.agents. a_agent import AAgent
-from pettingllms.multi_agent_env.deep_search.agents. rm_agent import RMAgent
-
-__all__ = ["QAgent", "RAgent", "AAgent", "RMAgent"]
